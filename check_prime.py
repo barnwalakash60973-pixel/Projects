9bb821f924b61540bc217835285ae875b977a876
barnwalakash60973-pixel <barnwalakash60973@gmail.com>
2026-01-04 19:38:35 +0530

    Improve comments in check_prime.py
    Added comments for clarity and corrected phrasing.

--- check_prime.py
+++ check_prime.py
@@ -1,4 +1,6 @@
 def check_prime_number(nums):
+    #Check how many prime number in between 1 to nums
+
 
     if (nums <= 1):
         print("No prime number exist")
@@ -6,9 +8,10 @@ def check_prime_number(nums):
     
     else:
         print("The prime numbers are:")
+        
         for i in range(2, nums+1):
 
-            check = False  #assume all is prime
+            check = False  #assume all are  prime numbers
 
             for j in range(2, (i//2 + 1)):
                 if (i % j == 0):
